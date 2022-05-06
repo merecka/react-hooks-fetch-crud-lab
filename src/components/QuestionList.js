28cfccca9e08a79a4ba39495fdcbfa176972c8b9
alex-merecka <amerecka@webcreek.com>
2022-05-06 11:35:32 -0500

    GET and POST working

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+
+function QuestionList({getQuestions, questions}) {
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => getQuestions(questions));
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question) => (
+          <QuestionItem key={question.id} question={question} />
+        ))}</ul>
     </section>
   );
 }
